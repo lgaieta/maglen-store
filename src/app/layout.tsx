d662fc40e2afa6ef1fb3deadf3cde29b48f99780
Luciano Aieta <108735782+lgaieta@users.noreply.github.com>
2023-05-12 15:13:46 +0000

    Started from mobile ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,20 @@
-import { ReactNode } from 'react';
-import './global.css';
+import { ReactNode } from "react";
+import "./global.css";
+import Link from "next/link";
 
 function Layout(props: { children: ReactNode }) {
-    return (
-        <html>
-            <body>{props.children}</body>
-        </html>
-    );
+  return (
+    <html>
+      <body className="flex flex-col w-full bg-white text-gray-900">
+        <header className="flex justify-between w-full py-4 px-4 md:px-64 border-b border-b-gray-200">
+          <Link className="font-bold" href="/">
+            MAGLEN
+          </Link>
+        </header>
+        {props.children}
+      </body>
+    </html>
+  );
 }
 
 export default Layout;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export const metadata = {
-    title: 'Inicio - MAGLEN STORE',
+  title: "Inicio - MAGLEN STORE",
 };
 
 function Home() {
-    return (
-        <main className='w-screen h-screen flex justify-center items-center text-slate-900'>
-            <h1 className='text-5xl font-bold text-center'>MAGLEN STORE</h1>
-        </main>
-    );
+  return (
+    <main className="w-full h-screen flex flex-col bg-white text-gray-900">
+      <section className="flex flex-col items-center justify-center w-full h-full">
+        <h1 className="text-5xl text-center font-bold z-10">
+          Elegí tu ropa preferida
+        </h1>
+        <p className="text-slate-600 text-md mt-4">
+          Ropa moderna para adolescentes
+        </p>
+        <Link
+          className="mt-6 px-8 py-3 text-md font-bold rounded-full transition-colors z-10 bg-gray-900 text-white hover:bg-gray-700"
+          href="#"
+        >
+          Ver ropa
+        </Link>
+      </section>
+    </main>
+  );
 }
 
 export default Home;
