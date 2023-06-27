08c7753de66c5720423e1eea9c6f1e0ec444aa56
lgaieta <lgaieta.dev@gmail.com>
2023-06-27 19:54:39 -0300

    Added image to home for Juarez

--- app/sections/HeroSection.tsx
+++ app/sections/HeroSection.tsx
@@ -1,18 +1,25 @@
 import { ButtonLink } from '@/components/Button';
-import Carousel from '@/components/Carousel';
+import Image from 'next/image';
 
 function HeroSection() {
     return (
-        <section className='flex flex-col'>
-            <div className='flex flex-col items-center justify-center gap-6'>
-                <Carousel />
-                <h1 className='text-[52px] text-center font-bold leading-none px-6 text-text-dark'>
+        <section className='flex m-auto max-w-[900px] w-full items-center justify-center gap-6'>
+            <div className='flex flex-col w-full gap-6 items-start'>
+                <h1 className='text-[52px] font-bold leading-none text-text-dark'>
                     Elegí tu ropa preferida
                 </h1>
-                <p className='text-center px-6'>
-                    Te ofrecemos ropa buena, bonita y barata.
-                </p>
-                <ButtonLink href='#'>COMPRAR</ButtonLink>
+                <p>Te ofrecemos ropa buena, bonita y barata.</p>
+                <ButtonLink href='/ropa'>COMPRAR</ButtonLink>
+            </div>
+            <div className='flex items-center justify-center w-full aspect-square'>
+                <div className='relative w-3/4 h-full'>
+                    <Image
+                        className='rounded-xl object-cover hover:scale-105 transition-transform'
+                        src='/girl.jpg'
+                        alt='Ropa en Maglen store'
+                        fill
+                    />
+                </div>
             </div>
         </section>
     );
