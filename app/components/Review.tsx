30991d0d7500295e163c45c31f90c87bbd388134
lgaieta <lgaieta.dev@gmail.com>
2023-07-22 15:06:01 -0300

    Added reviews to social proof section

---
+++ app/components/Review.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from 'react';
+
+type ReviewProps = {
+    author: string;
+    content: string;
+    image: ReactNode;
+};
+
+function Review(props: ReviewProps) {
+    const { author, content, image } = props;
+
+    return (
+        <li className='relative flex border border-text flex-col items-center text-center w-full h-fit gap-2 p-6 pt-10 rounded-lg'>
+            <div className='absolute rounded-full w-14 h-14 -top-7'>
+                {image}
+            </div>
+            <p>{content}</p>
+            <p>{author}</p>
+        </li>
+    );
+}
+
+export default Review;
