5a3f53e44f61c74f266fd77be8bae7f3beb44585
lgaieta <lgaieta.dev@gmail.com>
2023-10-31 14:05:37 -0300

    Added theme colors to footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 function Footer() {
     return (
-        <footer className='flex flex-col gap-4 bg-background-dark text-text justify-center items-center py-8 w-full border-t border-border'>
+        <footer className='flex flex-col gap-4 bg-content2 text-foreground justify-center items-center py-8 w-full border-t border-divider'>
             <Link href='#'>@maglen.store</Link>
             <Link href='#'>Envianos un mensaje</Link>
             <Link href='#'>maglen.store</Link>
