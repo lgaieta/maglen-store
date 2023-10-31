a450b4552802718572e9d54c51c27b2c637fbb37
lgaieta <lgaieta.dev@gmail.com>
2023-10-31 14:06:16 -0300

    Removed border from ProductForm

--- app/subir-ropa/page.tsx
+++ app/subir-ropa/page.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
 
 function CreateProductPage() {
     return (
-        <main className='flex flex-col items-center section-width-limits w-full border min-h-[90vh]'>
+        <main className='flex flex-col items-center section-width-limits w-full min-h-[90vh]'>
             <h1 className='text-3xl font-bold text-center py-10'>Subir ropa</h1>
             <ProductForm />
         </main>
